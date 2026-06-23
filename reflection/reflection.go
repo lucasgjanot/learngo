921c6d168c481d18e60f10747954aab524278f3d
F6322427 Lucas Galvao Janot <lucas.janot@bb.com.br>
2026-06-23 19:07:23 -0300

    reflection

---
+++ reflection/reflection.go
@@ -0,0 +1,37 @@
+package reflection
+
+import "reflect"
+
+func walk(x any, fn func(input string)) {
+	val := getValue(x)
+	
+	numberOfValues := 0
+	var getField func(int) reflect.Value
+	
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice:
+		numberOfValues = val.Len()
+		getField = val.Index
+	}
+
+	for i := range numberOfValues {
+		walk(getField(i).Interface(), fn)
+	}
+}
+
+
+
+func getValue(x any) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
+}
